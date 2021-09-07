8a628e85db6c123ffba95867ffe1f7d34fde3f5e
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2021-09-06 23:59:47 -0500

    collections -> Counter

---
+++ collections/main.py
@@ -0,0 +1,17 @@
+# Módulo Collections - Counter
+from collections import Counter
+# a = 2
+# b = 1
+# c = 1
+# f = 2
+# k = 3
+# i = 3
+vals = ["a", "b", "c", "f", "a", "f", "k", "i", "i", "i", "k", "k"]
+
+cuenta = Counter("murcielago")
+
+
+print(cuenta.most_common(3))
+
+
+
